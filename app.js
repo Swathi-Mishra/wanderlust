3195e353152ec8bb2d587d70b4853e0e1790076a
Swathi-Mishra <swathimishra150@gmail.com>
2024-06-22 16:00:43 +0530

    deleted 2 lines

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ const listingsRouter = require("./routes/listing.js");
 const reviewsRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-//const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbURL = process.env.ATLASDB_URL;
 
 
@@ -62,9 +61,6 @@ const sessionOptions = {
     }
 };
 
-// app.get("/", (req,res) => {
-//     res.send("root working");
-// });
 
 app.use(session(sessionOptions));
 app.use(flash());
